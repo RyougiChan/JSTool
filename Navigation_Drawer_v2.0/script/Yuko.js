510ac8191a96f32157d0ee6ecf45d99f3dfbb06b
RyougiChan <ryougi.chan.fire@gmail.com>
2017-08-12 13:50:25 +0800

    Rebuild html file and script file(Make a based fragment for Yuko.js)

---
+++ Navigation_Drawer_v2.0/script/Yuko.js
@@ -0,0 +1,32 @@
+//Yuko Mobile
+
+(function (win) {
+    "use strict";
+
+    var global = win;
+
+    if (!global.Yuko) {
+        global.Yuko = global.Yuko = global.yuko = Object();
+    }
+    
+    Yuko.utility = (function () {
+        
+    })();
+
+    Yuko.style = (function () {
+        
+    })();
+
+    Yuko.event = (function () {
+
+    })();
+
+    Yuko.effect = (function () {
+
+    })();
+
+    Yuko.widget = (function () {
+
+    })();
+
+})(window);
